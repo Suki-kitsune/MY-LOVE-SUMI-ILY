e16aff7b1b9a19272411f3951f35d28381daa8b6
Suki-kitsune <mayankprayal@gmail.com>
2026-07-12 14:36:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
+const envelope = document.getElementById("envelope");
 
+envelope.onclick = () => {
+
+envelope.classList.add("open");
+
+setTimeout(()=>{
+
+document.body.style.transition="1s";
+
+document.body.style.opacity="0";
+
+setTimeout(()=>{
+
+window.location.href="letter.html";
+
+},1000);
+
+},1400);
+
+};
+
+
+function createPetal(){
+
+const petal=document.createElement("div");
+
+petal.className="petal";
+
+petal.innerHTML="🌸";
+
+petal.style.left=Math.random()*100+"vw";
+
+petal.style.animationDuration=4+Math.random()*5+"s";
+
+petal.style.fontSize=18+Math.random()*18+"px";
+
+document.querySelector(".petals").appendChild(petal);
+
+setTimeout(()=>{
+
+petal.remove();
+
+},9000);
+
+}
+
+setInterval(createPetal,350);
